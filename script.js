d3860c4dd854312c26e06e2b9940928cf971a943
AsraZabeen <asrazabeen13@gmail.com>
2026-06-24 17:59:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+let string = "";
+let buttons = document.querySelectorAll('.button');
+Array.from(buttons).forEach((button) => {
+    button.addEventListener('click', (e) => {
+        if (e.target.innerHTML == '=') {
+            string = eval(string);
+            document.querySelector('input').value = string
+        }
+        else if (e.target.innerHTML == 'AC') {
+            string = "";
+            document.querySelector('input').value = string
+        }
+        else {
+            console.log(e.target)
+            string = string + e.target.innerHTML;
+            document.querySelector('input').value = string
+
+        }
+
+        function CalculateResults(){
+            try{
+                if(e.target.innerHTML == '+/-'){
+                    throw new Error("invalid symbol")
+                }
+            }
+            catch (Error){
+                document.querySelector('input').value = "Error";
+
+            }
+
+        }
+
+    })
+})
